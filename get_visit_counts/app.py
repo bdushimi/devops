7831e8dd1ff7ff713f4743bc501c0b7055df3dcb
bdushimi <lespharaons@hotmail.com>
2024-04-27 00:22:24 +0200

    Refactors lambda functions

--- get_visit_counts/app.py
+++ get_visit_counts/app.py
@@ -4,30 +4,12 @@ import boto3
 
 def lambda_handler(event, context):
     print("get_visit_counts function is called")
-
     try:
         body = json.loads(event['body'])
         appName = body["app-name"]
-        visit_count: int = 0
-
-        # Create a DynamoDB Client & Connection
-        dynamodb = boto3.resource('dynamodb')
-        table_name = "apps-visit-counts"
-        table = dynamodb.Table(table_name)
-
-        # Get the apps visit count from dynamodb
-        response = table.get_item(Key={"app-name": appName})
-
-        if "Item" in response:
-            if "count" in response["Item"]:
-                visit_count = response["Item"]["count"]
-
-        # Increment the number of visits
-        visit_count += 1
-
-        # Update the number of visits in dynamodb
-        table.put_item(Item={"app-name": appName, "count": visit_count})
-
+        
+        visit_count = update_visit_counts(appName)
+        
         response_data = {
             'app-name': f"{appName}",
             'visitCount': f"{visit_count}"
@@ -52,3 +34,33 @@ def lambda_handler(event, context):
                 "exception": str(e)
             }
         }
+
+
+def update_visit_counts(appName):
+    
+    try:
+        # Initialize the visit count
+        visit_count: int = 0
+        
+        # Create a DynamoDB Client & Connection
+        dynamodb = boto3.resource('dynamodb')
+        table_name = "apps-visit-counts"
+        table = dynamodb.Table(table_name)
+
+        # Get the apps visit count from dynamodb
+        response = table.get_item(Key={"app-name": appName})
+        
+        if "Item" in response:
+            if "count" in response["Item"]:
+                visit_count = response["Item"]["count"]
+
+         # Increment the number of visits
+        visit_count += 1
+
+        # Update the number of visits in dynamodb
+        table.put_item(Item={"app-name": appName, "count": visit_count})
+        
+        return visit_count
+
+    except Exception as e:
+        raise e
